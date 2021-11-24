60a26e4dc1a69a286df38e1a526e2a857ef17cf1
Nicolás Gómez <nicolasgomezespejo@gmail.com>
2021-11-24 13:06:19 -0300

    handle real fhir server

--- src/fhir/index.js
+++ src/fhir/index.js
@@ -1,8 +1,35 @@
-exports.getBradenAssessmentPanel = async function getBradenAssessmentPanel (patientId) {
+const fetch = require('node-fetch')
+
+const BRADEN_ASSESSMENT_PANNEL_CODE = 'http://loinc.org|38228-3'
+
+const buildBradenPanelURL = (fhirServer, patientId) =>
+  `${fhirServer}/DiagnosticReport?patient=${patientId}&code=${BRADEN_ASSESSMENT_PANNEL_CODE}&_sort:desc=date&_count=1`;
+
+function buildMockResponse() {
   const examples = [
     require('./examples/high-risk.json'),
     require('./examples/low-risk.json'),
     require('./examples/moderate-risk.json')
   ]
   return examples[Math.floor(Math.random() * examples.length)]
+}
+
+exports.getBradenAssessmentPanel = async function getBradenAssessmentPanel (fhirServer, patientId) {
+  if (!fhirServer) {
+    return buildMockResponse()
+  }
+
+  const response = await fetch(buildBradenPanelURL(fhirServer, patientId))
+
+  if (response.ok) {
+    const bundleJson = await response.json()
+
+    if (bundleJson.total === 0) {
+      return null;
+    }
+
+    return bundleJson.entry[0].resource;
+  }
+
+  return null;
 }
--- src/server.js
+++ src/server.js
@@ -11,17 +11,17 @@ const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(bodyParser.json())
 
-const BRADEN_SERVICE = {
-  name: 'Braden CDS Hook Demo',
-  title: 'Braden CDS Hook Demo',
-  id: 'braden-prescribe',
+const LPP_SERVICE = {
+  name: 'LPP CDS Hook Demo',
+  title: 'LPP CDS Hook Demo',
+  id: 'lpp-prevention-recommendations',
   description: 'Provide recommendations for patients at risk of pressure injuries',
   hook: 'patient-view'
 }
 
 const SERVICES = {
   services: [
-    BRADEN_SERVICE
+    LPP_SERVICE
   ]
 }
 
@@ -30,10 +30,10 @@ app.get('/cds-services', (req, res) => {
 })
 
 app.get('/', (req, res) => {
-  res.send('Braden CDS Hook Running!')
+  res.send('LPP CDS Hook Running!')
 })
 
-app.post(`/cds-services/${BRADEN_SERVICE.id}`, asyncHandler(async (req, res) => {
+app.post(`/cds-services/${LPP_SERVICE.id}`, asyncHandler(async (req, res) => {
   const { fhirServer, context: { patientId } } = req.body
 
   const panel = await getBradenAssessmentPanel(fhirServer, patientId)
